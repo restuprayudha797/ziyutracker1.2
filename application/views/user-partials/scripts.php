0b0ca3808120ee0e8082a22f1a0d6d391450e01a
restuprayudha797 <webmerdeka2911@gmail.com>
2023-12-03 01:03:28 +0700

    route

---
+++ application/views/user-partials/scripts.php
@@ -0,0 +1,54 @@
+  <!-- Back-to-top -->
+  <a href="#top" id="back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+  <!-- Jquery-scripts -->
+  <script src="<?= base_url('assets/root') ?>/js/vendors/jquery-3.2.1.min.js"></script>
+
+  <!-- Moment js-->
+  <script src="<?= base_url('assets/root') ?>/plugins/moment/moment.min.js"></script>
+
+  <!-- Bootstrap-scripts js -->
+  <script src="<?= base_url('assets/root') ?>/js/vendors/bootstrap.bundle.min.js"></script>
+
+  <!-- Sparkline JS-->
+  <script src="<?= base_url('assets/root') ?>/js/vendors/jquery.sparkline.min.js"></script>
+
+  <!-- Bootstrap-daterangepicker js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+  <!-- Bootstrap-datepicker js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/bootstrap-datepicker/bootstrap-datepicker.js"></script>
+
+  <!-- Chart-circle js -->
+  <script src="<?= base_url('assets/root') ?>/js/vendors/circle-progress.min.js"></script>
+
+  <!-- Rating-star js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/rating/jquery.rating-stars.js"></script>
+
+  <!-- Custom scroll bar js-->
+  <script src="<?= base_url('assets/root') ?>/plugins/scroll-bar/jquery.mCustomScrollbar.concat.min.js"></script>
+
+  <!-- Nice-select js-->
+  <script src="<?= base_url('assets/root') ?>/plugins/jquery-nice-select/js/jquery.nice-select.js"></script>
+  <script src="<?= base_url('assets/root') ?>/plugins/jquery-nice-select/js/nice-select.js"></script>
+
+  <!-- P-scroll js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/p-scroll/p-scroll.js"></script>
+  <script src="<?= base_url('assets/root') ?>/plugins/p-scroll/p-scroll-horizontal.js"></script>
+
+  <!-- Sidemenu js-->
+  <script src="<?= base_url('assets/root') ?>/plugins/sidemenu/sidemenu.js"></script>
+
+  <!-- Sidemenu-respoansive-tabs js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/sidemenu-responsive-tabs/js/sidemenu-responsive-tabs.js"></script>
+
+  <!-- Horizontal js-->
+  <script src="<?= base_url('assets/root') ?>/plugins/horizontal-menu/horizontal.js"></script>
+
+  <!-- Rightsidebar js -->
+  <script src="<?= base_url('assets/root') ?>/plugins/sidebar/sidebar.js"></script>
+
+
+
+  <!-- Custom js -->
+  <script src="<?= base_url('assets/root') ?>/js/custom.js"></script>
---
+++ application/views/user/dasbor.php
@@ -0,0 +1,108 @@
+<!-- row opened -->
+<div class="row">
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="card overflow-hidden">
+                  <div class="card-body">
+                    <div class="d-flex">
+                      <div class="">
+                        <p class="mb-2 h6">Active Client</p>
+                        <h2 class="mb-1">253</h2>
+                        <p class="mb-0 text-muted">
+                          <span class="text-success"
+                            >(+0.35%)<i
+                              class="fe fe-arrow-up text-success"
+                            ></i></span
+                          >Increase
+                        </p>
+                      </div>
+                      <div class="my-auto ml-auto">
+                        <div class="chart-wrapper text-center">
+                          <canvas
+                            id="areaChart1"
+                            class="areaChart2 chartjs-render-monitor chart-dropshadow-primary overflow-hidden mx-auto"
+                          ></canvas>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="card overflow-hidden">
+                  <div class="card-body">
+                    <div class="d-flex">
+                      <div class="">
+                        <p class="mb-2 h6">Total Revenue</p>
+                        <h2 class="mb-1">$365</h2>
+                        <p class="mb-0 text-muted">
+                          <span class="text-danger">(+0.54%)</span
+                          ><i class="fe fe-arrow-down text-danger"></i>Decrease
+                        </p>
+                      </div>
+                      <div class="my-auto ml-auto">
+                        <div class="chart-wrapper">
+                          <canvas
+                            id="areaChart2"
+                            class="areaChart2 chartjs-render-monitor chart-dropshadow-secondary overflow-hidden"
+                          ></canvas>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="card overflow-hidden">
+                  <div class="card-body">
+                    <div class="d-flex">
+                      <div class="">
+                        <p class="mb-2 h6">Sales</p>
+                        <h2 class="mb-1">15,89</h2>
+                        <p class="mb-0 text-muted">
+                          <span class="text-success"
+                            >(+0.96%)<i
+                              class="fe fe-arrow-up text-success"
+                            ></i></span
+                          >Increase
+                        </p>
+                      </div>
+                      <div class="my-auto ml-auto">
+                        <div class="chart-wrapper">
+                          <canvas
+                            id="areaChart3"
+                            class="areaChart3 chartjs-render-monitor chart-dropshadow-info overflow-hidden"
+                          ></canvas>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="card overflow-hidden">
+                  <div class="card-body">
+                    <div class="d-flex">
+                      <div class="">
+                        <p class="mb-2 h6">Running Projects</p>
+                        <h2 class="mb-1">25</h2>
+                        <p class="mb-0 text-muted">
+                          <span class="text-danger">(+0.42%)</span
+                          ><i class="fe fe-arrow-down text-danger"></i>Decrease
+                        </p>
+                      </div>
+                      <div class="my-auto ml-auto">
+                        <div class="chart-wrapper">
+                          <canvas
+                            id="areaChart4"
+                            class="areaChart4 chartjs-render-monitor chart-dropshadow-success overflow-hidden"
+                          ></canvas>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- row closed -->
+
+          
