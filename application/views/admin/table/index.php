c923fc39a3dcc0be7e2ee348bc6f548553fc861e
unknown <ridhokufniawan@gmail.com>
2023-12-03 01:09:40 +0700

    .

---
+++ application/views/admin/table/index.php
@@ -0,0 +1,44 @@
+<!-- row opened -->
+<div class="row">
+							<div class="col-md-12 col-lg-12">
+								<div class="card">
+									<div class="card-header">
+										<div class="card-title">Sample Data Table</div>
+										<div class="card-options">
+											<a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+											<a href="#" class="card-options-fullscreen" data-toggle="card-fullscreen"><i class="fe fe-maximize"></i></a>
+											<a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
+										</div>
+									</div>
+									<div class="card-body">
+										<div class="table-responsive ">
+											<table id="example-2" class="table table-striped table-bordered text-nowrap">
+												<thead>
+													<tr>
+														<th class="wd-15p border-bottom-0">First name</th>
+														<th class="wd-15p border-bottom-0">Last name</th>
+														<th class="wd-20p border-bottom-0">Position</th>
+														<th class="wd-15p border-bottom-0">Start date</th>
+														<th class="wd-10p border-bottom-0">Salary</th>
+														<th class="wd-25p border-bottom-0">E-mail</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>agus</td>
+														<td>Chloe</td>
+														<td>System Developer</td>
+														<td>2018/03/12</td>
+														<td>$654,765</td>
+														<td>b.Chloe@datatables.net</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+									<!-- table-wrapper -->
+								</div>
+								<!-- section-wrapper -->
+							</div>
+						</div>
+						<!-- row closed -->
