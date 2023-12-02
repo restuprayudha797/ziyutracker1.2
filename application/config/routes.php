1b7740bb645603c0a0ec86782b22257a5f5f80f4
restuprayudha797 <webmerdeka2911@gmail.com>
2023-12-02 20:46:44 +0700

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 $route = array(
-    'beranda' => 'Home',
+    'beranda' => 'home/home',
     'data-user' => 'admin/admin/page_users',
     'login' => 'auth/auth',
     'keluar' => 'logout'
@@ -59,6 +59,6 @@ $route = array(
 );
 
 
-$route['default_controller'] = 'Home';
+$route['default_controller'] = 'home/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
