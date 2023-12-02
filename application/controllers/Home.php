dc273570d49cd492007c237acb0d2662b0c1d15e
restuprayudha797 <webmerdeka2911@gmail.com>
2023-12-02 18:38:24 +0700

    up to date autoload file

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,8 @@ class Home extends CI_Controller{
 
     public function index(){
 
-        $this->load->view('home-index');
+        $data['title'] = 'home';
+        $this->load->view('home-index', $data);
     }
 
 
