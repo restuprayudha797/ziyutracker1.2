8b8395bb70c3f4f26f915989209e321ffbde4640
restuprayudha797 <webmerdeka2911@gmail.com>
2023-12-03 04:38:00 +0700

    led status

--- application/controllers/user/Saklar.php
+++ application/controllers/user/Saklar.php
@@ -25,23 +25,19 @@ class Saklar extends CI_Controller
 
         if ($id_devices_active != null) {
             $user = $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array();
-            $userActive = $this->db->get_where('devices_active', ['email' => $user['email']])->row_array();
-            $marker  = $this->db->query("SELECT * FROM `marker_" . $id_devices_active . "` ORDER BY id_marker DESC LIMIT 1;")->row_array();
 
-            $user = $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array();
-            $userActive = $this->db->get_where('user_active', ['email' => $user['email']])->row_array();
-            $ledStatus  = $this->db->get('ledStatus_' . $userActive['id_active'])->result_array();
-
+           
+            $ledstatus =  $this->db->query("SELECT * FROM `ledstatus_" . $id_devices_active . "`")->row_array();
+          
 
             $data = [
                 'title' => 'Location',
                 'page' => 'saklar',
                 'user' => $user,
-                'marker' => $marker
+                'ledStatus' => $ledstatus
             ];
 
             $this->load->view('user-index', $data);
-        } else {
         }
     }
 
--- application/views/user/saklar.php
+++ application/views/user/saklar.php
@@ -4,16 +4,16 @@
             <div class="avatar-list p-5">
                 <center>
 
-                    <?php if ($ledStatus[0]['state'] == 1) : ?>
+                    <?php if ($ledStatus['state'] == 1) : ?>
                         <a href="<?= base_url('off') ?>">
-                            <img src="<?= base_url('front_ass/icon/on.png') ?>" alt="" width="140px">
+                            <img src="<?= base_url('assets/root/icon/on.png') ?>" alt="" width="140px">
                         </a>
                         <h1 class="mt-3">Hidup</h1>
 
 
                     <?php else : ?>
                         <a href="<?= base_url('on') ?>">
-                            <img src="<?= base_url('front_ass/icon/off.png') ?>" alt="" width="140px">
+                            <img src="<?= base_url('assets/root/icon/off.png') ?>" alt="" width="140px">
                         </a>
                         <h1 class="mt-3">Mati</h1>
                     <?php endif ?>
